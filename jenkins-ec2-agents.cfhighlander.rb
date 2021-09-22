27796de3c44109cbda9efe6d5f83ecf76238ed0a
Guslington <guslington@gmail.com>
2021-09-22 13:11:45 +1000

    bump iam lib version for multi statement policy support

--- jenkins-ec2-agents.cfhighlander.rb
+++ jenkins-ec2-agents.cfhighlander.rb
@@ -2,7 +2,7 @@ CfhighlanderTemplate do
   Name 'jenkins-ec2-agents'
   Description "jenkins-ec2-agents - #{component_version}"
   
-  DependsOn 'lib-iam@0.1.0'
+  DependsOn 'lib-iam@0.2.0'
   
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
